0610054b0bca00d5cf47f48db9cb58665e9320fa
andreosenna <andreosenna@gmail.com>
2019-09-17 18:28:00 -0300

    implementação do findbyDepartment com map

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.Date;
+import java.util.List;
 
 import model.DAO.DaoFactory;
 import model.DAO.SellerDAO;
@@ -13,6 +14,13 @@ public class Program {
 		SellerDAO sellerDao = DaoFactory.createSellerDAO();
 		Seller seller = sellerDao.findById(2);
 		System.out.println(seller);
+		
+		System.out.println("+========++=======++=====++=====+++=========");
+		Department dep = new Department(4,null);
+		List<Seller> sel = sellerDao.findByDepartment(dep);
+		for(Seller obj : sel) {
+			System.out.println(obj);
+		}
 	}
 
 }
